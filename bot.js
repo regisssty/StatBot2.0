2565a09c7eb102962a816a612af7ba409423c7cb
regisssty <mamameep@live.com>
2017-11-25 23:24:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,18 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '/ping') {
+    	message.channel.sendMessage('pong');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '/bot') {
+    	message.reply('Here are the current bot hosted by @Rëgîš#6434.\n__*Moderation*__\n@ModBot#3385\n\n__*MusicBot*__\n@Bonlicious#3934\n@DELICIOUS#5285\n@RegisBot#6361\n@SpeakBot#2502\n@SpeakBot#2589\n@SpeakBot#2812\n@BON BOT SLAVE#2874\n\n__*GameTracker*__\n@GameTracker#0585');
+  	}
+});
+client.on('message', message => {
+    if (message.content === '/version') {
+    	message.channel.sendMessage('**StatBot is running on JavaScript.**\n**Version: 0.4.2**');
   	}
 });
 
