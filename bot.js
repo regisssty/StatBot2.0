6b51408708a3969f03b2fe6d069023afc92d3a12
regisssty <mamameep@live.com>
2017-11-25 23:51:51 +0800

    Removed Uptime for now

--- bot.js
+++ bot.js
@@ -20,29 +20,7 @@ client.on('message', message => {
     	message.channel.sendMessage('**StatBot is running on JavaScript.**\n**Version: 0.4.2**');
   	}
 });
-// Uptime
-client.on('message', message => {
- if (message.content === '<uptime') {
-    var time = process.uptime();
-function dhm(t){
-var cd = 24 * 60 * 60 * 1000,
-    ch = 60 * 60 * 1000,
-    d = Math.floor(t / cd),
-    h = Math.floor( (t - d * cd) / ch),
-    m = Math.round( (t - d * cd - h * ch) / 60000),
-    pad = function(n){ return n < 10 ? '0' + n : n; };
-  if( m === 60 ){
-h++;
-m = 0;
-  }
-  if( h === 24 ){
-d++;
-h = 0;
-  }
-  return [d, pad(h), pad(m)].join(':');
-}
-  message.channel.sendMessage(dhm(time)); 
- }});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
