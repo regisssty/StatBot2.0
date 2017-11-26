99f24a6ea847a43b4b3dbf2dd22a7753a7f9aa7b
regisssty <mamameep@live.com>
2017-11-26 16:51:53 +0800

    ??

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 //const config = require('./config.json');
 
 client.on('ready', () => {
-    client.user.setGame(game, ['Help command: <help']);
+    client.user.setPresence({game: {name: "Help command: <help", type: 0}});
     console.log('I am ready!');
 });
 
