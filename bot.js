919e7eef2fa23b887cf815af860e243a5171d5b0
regisssty <mamameep@live.com>
2017-11-26 16:20:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-//const yt = require('ytdl-core');
-//const config = require('./config.json');
+const yt = require('ytdl-core');
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
